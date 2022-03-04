14e47b72437995efa3951d4c76f5a181c3c69fc6
Alisson de Souza <amlsalisson@gmail.com>
2022-03-04 16:48:01 -0300

    api test

--- src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -45,7 +45,7 @@ public class APITest {
 		.then()
 			.log().all()
 			.statusCode(400)
-			.body("message",CoreMatchers.is("Due cdsdate must not be in past"))			
+			.body("message",CoreMatchers.is("Due date must not be in past"))			
 		;
 	}	
 }
